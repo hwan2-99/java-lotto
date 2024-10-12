aef1a9ff568f4e994f8cb7139e242f7ef9ff4f4c
hwan2-99 <thh1233123@gmail.com>
2024-10-12 10:28:17 +0900

    refactor: 불필요한 stream문제거

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -8,15 +8,13 @@ import java.util.Map;
 
 public class LottoResult {
     private static final int DEFAULT_VALUE = 0;
-    private static final int PLUS_VALUE = 1;
 
     private final Map<LottoReward, Integer> resultMap;
 
     public LottoResult(final List<LottoReward> lottoRewards) {
         resultMap = new EnumMap<>(LottoReward.class);
-        Arrays.stream(LottoReward.values()).forEach(lottoReward -> resultMap.put(lottoReward, DEFAULT_VALUE));
         for (LottoReward lottoReward : lottoRewards) {
-            resultMap.replace(lottoReward, resultMap.get(lottoReward) + PLUS_VALUE);
+            resultMap.put(lottoReward,Collections.frequency(lottoRewards, lottoReward));
         }
     }
 
